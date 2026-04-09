1349a7276f941ad4ab9d951c7414efacce9927d5
Sebastien Lavayssiere <sebastien.lavayssiere@zenika.com>
2026-04-09 14:09:00 +0200

    Refresh tests logs

--- frontend/dist/assets/fingerprint-B-oCWclK.js
+++ frontend/dist/assets/fingerprint-B-oCWclK.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BMIbNxLQ.js";/**
+import{c as a}from"./index-DY2FiiYI.js";/**
  * @license lucide-vue-next v0.300.0 - ISC
  *
  * This source code is licensed under the ISC license.
